78a16814a878ef04f73733c48e6d583b4cf669e8
Nawon <nawon2794@gmail.com>
2025-07-24 23:24:07 +0530

    Updated Order in Order panel of Admin Page✏️

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -5,28 +5,35 @@ import { FaFileInvoice } from "react-icons/fa6";
 import AdminProductsPage from "./admin/AdminProductsPage";
 import AddProductForm from "./admin/AddProductForm";
 import EditProduct from "./admin/EditProduct";
-
+import AdminOrdersPage from "./admin/AdminOrdersPage";
 
 const AdminPage = () => {
     return (
         <div className="w-full h-screen bg-gray-200 flex p-2">
             <div className="h-full w-[300px] ">
-                <Link to="/admin/users" className=" p-2 flex items-center"><FaUsers className="mr-2" />Users</Link>
-                <Link to="/admin/products" className=" p-2 flex items-center"><FaWarehouse className="mr-2" />Products</Link>
-                <Link to="/admin/orders" className=" p-2 flex items-center"><FaFileInvoice className="mr-2" />Orders</Link>
+                <Link to="/admin/users" className=" p-2 flex items-center">
+                    <FaUsers className="mr-2" />
+                    Users
+                </Link>
+                <Link to="/admin/products" className=" p-2 flex items-center">
+                    <FaWarehouse className="mr-2" />
+                    Products
+                </Link>
+                <Link to="/admin/orders" className=" p-2 flex items-center">
+                    <FaFileInvoice className="mr-2" />
+                    Orders
+                </Link>
             </div>
             <div className="h-full bg-white w-[calc(100vw-300px)] rounded-lg">
                 <Routes path="/*">
-                    <Route path="/users" element={<h1>Users</h1>}/>
-                    <Route path="/products" element={<AdminProductsPage/>}/>
-                    <Route path="/orders" element={<h1>Orders</h1>}/>
-                    <Route path="/addProduct" element={<AddProductForm/>}/>
-                    <Route path="/editProduct" element={<EditProduct/>}/>
-                    
+                    <Route path="/users" element={<h1>Users</h1>} />
+                    <Route path="/products" element={<AdminProductsPage />} />
+                    <Route path="/orders" element={<AdminOrdersPage />} />
+                    <Route path="/addProduct" element={<AddProductForm />} />
+                    <Route path="/editProduct" element={<EditProduct />} />
                 </Routes>
             </div>
         </div>
-        
     );
 };
 
--- src/pages/admin/AdminProductsPage.jsx
+++ src/pages/admin/AdminProductsPage.jsx
@@ -6,107 +6,102 @@ import { GrEdit } from "react-icons/gr";
 import { Link, useNavigate } from "react-router-dom";
 import Loader from "../../components/Loader";
 
-
 const AdminProductsPage = () => {
+    const [products, setProducts] = useState([]);
+    const [loaded, setLoaded] = useState(false);
+    const navigate = useNavigate();
 
-    const [products, setProducts] = useState([])
-    const [loaded, setLoaded] = useState(false)
-    const navigate = useNavigate()
-
-    useEffect(()=>{
-        if(!loaded){
-            axios.get(import.meta.env.VITE_BACKEND_URL+"/api/product").then(
-            (response)=>{
+    useEffect(() => {
+        if (!loaded) {
+            axios.get(import.meta.env.VITE_BACKEND_URL + "/api/product").then((response) => {
                 //console.log(response.data)
-                setProducts(response.data)
-                setLoaded(true)
-                }
-            )
+                setProducts(response.data);
+                setLoaded(true);
+            });
         }
-    },[loaded])
+    }, [loaded]);
 
-    async function deleteProduct(id){
-        const token = localStorage.getItem("token")
-        if(token==null){
+    async function deleteProduct(id) {
+        const token = localStorage.getItem("token");
+        if (token == null) {
             toast.error("Please login to delete a product");
-            return
+            return;
         }
-        try{
-            await axios.delete(import.meta.env.VITE_BACKEND_URL+"/api/product/"+id,{
-            headers:{
-                Authorization:"Bearer "+token
-                }
-            })
-            setLoaded(false)
-            toast.success("Product deleted successfully")
-        }catch(error){
+        try {
+            await axios.delete(import.meta.env.VITE_BACKEND_URL + "/api/product/" + id, {
+                headers: {
+                    Authorization: "Bearer " + token,
+                },
+            });
+            setLoaded(false);
+            toast.success("Product deleted successfully");
+        } catch (error) {
             console.log(error);
-            toast.error("Error deleting product")
+            toast.error("Error deleting product");
         }
     }
 
-  
-    
-
     return (
-        <div className="w-full h-full rounded-lg relative" >
-            <Link to={"/admin/addProduct"} className="text-white absolute bg-gray-700 p-[12px] text-3xl rounded-full cursor-pointer hover:bg-gray-300 hover:text-gray-700 right-5 bottom-5">
+        <div className="w-full h-full rounded-lg relative">
+            <Link
+                to={"/admin/addProduct"}
+                className="text-white absolute bg-gray-700 p-[12px] text-3xl rounded-full cursor-pointer hover:bg-gray-300 hover:text-gray-700 right-5 bottom-5"
+            >
                 <FaPlus />
             </Link>
 
-            {loaded&&<table className="w-full">
-                
-                <thead>
-                    <tr>
-                        <th className="p-2">ProductID</th>
-                        <th className="p-2">Name</th>
-                        <th className="p-2">Price</th>
-                        <th className="p-2">Labeld Price</th>
-                        <th className="p-2">Stock</th>
-                        <th className="p-2">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {
-                products.map(
-                    (product, index)=>{
-                        return(
-                            <tr key={index} className="border-b-2 border-gray-300 text-center cursor-pointer hover:bg-gray-100">
-                                <td className="p-2">{product.productID}</td>
-                                <td className="p-2">{product.name}</td>
-                                <td className="p-2">{product.price}</td>
-                                <td className="p-2">{product.labeledPrice}</td>
-                                <td className="p-2">{product.stock}</td>
-                                <td className="p-2">
-                                    <div className="w-full h-full flex justify-center">
-                                        <FaRegTrashAlt onClick={()=>{
-                                            deleteProduct(product.productID)
-                                        }} className="text-[25px]  m-[10px] hover:text-red-500"/>
-                                        <GrEdit 
-                                        onClick={
-                                            ()=>{
-                                                //Load edit product form
-                                                navigate("/admin/editProduct",{
-                                                    state:product
-                                                })
-                                            }
-                                        }
-                                        className="text-[25px] m-[10px] hover:text-blue-500"/>
-                                    </div>
-                                </td>
-                            </tr>
-                            )
-                        }
-                    )
-                }
-                </tbody>
-                
-            </table>}
-            {
-                !loaded&&<Loader/>
-            }
+            {loaded && (
+                <table className="w-full">
+                    <thead>
+                        <tr>
+                            <th className="p-2">ProductID</th>
+                            <th className="p-2">Name</th>
+                            <th className="p-2">Price</th>
+                            <th className="p-2">Labeld Price</th>
+                            <th className="p-2">Stock</th>
+                            <th className="p-2">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {products.map((product, index) => {
+                            return (
+                                <tr
+                                    key={index}
+                                    className="border-b-2 border-gray-300 text-center cursor-pointer hover:bg-gray-100"
+                                >
+                                    <td className="p-2">{product.productID}</td>
+                                    <td className="p-2">{product.name}</td>
+                                    <td className="p-2">{product.price}</td>
+                                    <td className="p-2">{product.labeledPrice}</td>
+                                    <td className="p-2">{product.stock}</td>
+                                    <td className="p-2">
+                                        <div className="w-full h-full flex justify-center">
+                                            <FaRegTrashAlt
+                                                onClick={() => {
+                                                    deleteProduct(product.productID);
+                                                }}
+                                                className="text-[25px]  m-[10px] hover:text-red-500"
+                                            />
+                                            <GrEdit
+                                                onClick={() => {
+                                                    //Load edit product form
+                                                    navigate("/admin/editProduct", {
+                                                        state: product,
+                                                    });
+                                                }}
+                                                className="text-[25px] m-[10px] hover:text-blue-500"
+                                            />
+                                        </div>
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            )}
+            {!loaded && <Loader />}
         </div>
     );
- };
+};
 
 export default AdminProductsPage;
